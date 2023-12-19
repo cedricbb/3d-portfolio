14d0ceb5a5e1038b83d433f4e5cea1492b4986df
cbillard <cbillard@com-company.fr>
2023-12-19 14:19:13 +0100

    Added stars in contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motion"
 
+
 const Contact = () => {
   const formRef = useRef()
   const [form, setForm] = useState({
